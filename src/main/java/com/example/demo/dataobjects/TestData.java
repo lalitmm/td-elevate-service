6902128cb4418a4351bbfabd56e81f62c8cac937
Nisha <patricia.nisha9@gmail.com>
2018-09-22 18:08:34 -0400

    list of service provides

---
+++ src/main/java/com/example/demo/dataobjects/TestData.java
@@ -0,0 +1,24 @@
+package com.example.demo.dataobjects;
+
+public class TestData {
+
+	private String service_id;
+
+	public String getService_id() {
+		return service_id;
+	}
+
+	public void setService_id(String service_id) {
+		this.service_id = service_id;
+	}
+
+	public String getService_name() {
+		return service_name;
+	}
+
+	public void setService_name(String service_name) {
+		this.service_name = service_name;
+	}
+
+	private String service_name;
+}
