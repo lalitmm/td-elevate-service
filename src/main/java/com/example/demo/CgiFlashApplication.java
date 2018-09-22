3ab75e81efe4d1e97a93fb23df7c15306938e68b
Nisha <patricia.nisha9@gmail.com>
2018-09-22 16:29:12 -0400

    project structure

---
+++ src/main/java/com/example/demo/CgiFlashApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgiFlashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgiFlashApplication.class, args);
+	}
+}
